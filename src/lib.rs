70eba0e0ecc89aaf5f9f1de60f3bab561d52424a
Tomoya Ishii <shi2gx@gmail.com>
2021-03-05 18:50:02 +0900

    Remove macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![warn(clippy::all)]
 pub extern crate __procon_complex as complex;
-pub extern crate __procon_input as input;
 pub extern crate __procon_itertools as itertools;
 pub extern crate __procon_math as math;
 pub extern crate __procon_math_traits as math_traits;
